b9cac8e8edeabf9b58509c045b4517b2fb39a8c2
Ryan Sheatsley <ryan@sheatsley.me>
2019-09-04 18:18:44 -0400

    cpu percentage

--- profile.py
+++ profile.py
@@ -1,11 +1,54 @@
 """
-
 Prints various performance metrics of your system in real-time
-
 """
 
+import numpy as np
+import subprocess
+
+CPU_STAT = [
+    "cat",
+    "/proc/stat",
+    "|",
+    "grep",
+    "'cpu[0-9]'",
+    "|",
+    "awk" "{'print",
+    "$2",
+    "$3",
+    "$4",
+    "$5",
+    "$6",
+    "$7",
+    "$8",
+    "$9",
+    "$10}",
+]
+
+
 def profile():
-        """
-        Prints CPU, RAM, and GPU utilization
-        """
-        
+    """
+    Prints CPU, RAM, and GPU utilization
+    """
+    return
+
+
+def cpu_percentage(old, new):
+    """
+    Compute CPU usage as a percentage from /proc/stat via:
+    total = user + nice + system + idle + iowait + irq + softirq + steal
+    nbusy = idle + iowait
+    usage = total - nbusy
+    percentage = usage / total * 100
+    """
+    user, nice, system, idle, iowait, irq, softirq, steal = np.subtract(old, new)
+    total = user + nice + system + idle + iowait + irq + softirq + steal
+    nbusy = idle + iowait
+    usage = total - nbusy
+    return usage / total * 100
+
+
+if __name__ == "__main__":
+
+    # grab initial CPU stats
+
+    return
