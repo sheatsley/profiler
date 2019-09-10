ba22331a36e25e0e23cb1c81743b47772107c016
Ryan Sheatsley <ryan@sheatsley.me>
2019-09-10 11:23:58 -0400

    initial release version

--- profile.py
+++ profile.py
@@ -12,70 +12,81 @@ class PerformanceProfiler:
     from math import ceil
     from numpy import array, nan_to_num, subtract
     from subprocess import check_output
-    from sys import stdout
+    import sys
     from time import time
     from threading import current_thread, Thread
 
     # profile commands & constants
-    self.CPU_STAT = [
-        ["cat /proc/stat"],
+    CPU_STAT = [
+        ["cat", "/proc/stat"],
         ["grep", "cpu[0-9]"],
         ["awk", "{print $2, $3, $4, $5, $6, $7, $8, $9}"],
     ]
-    self.GPU_COL = ["GPU0", "VRAM", "TEMP", "DRAM"]
-    self.GPU_STAT = [
+    GPU_COL = ["GPU0 ", "VRAM ", "TEMP ", " DRAM "]
+    GPU_STAT = [
         ["nvidia-smi"],
         ["grep", "Default"],
         ["awk", "{print $3, $9, $11, $13}"],
     ]
-    self.MEM_STAT = [["free -m"], ["grep Mem"], ["awk", "{print $2, $3}"]]
-    self.NUM_CORE = "nproc"
-    self.STAT_COL = []
+    MEM_STAT = [["free", "-m"], ["grep", "Mem"], ["awk", "{print $3, $4}"]]
+    NUM_CORE = ["nproc"]
+    STAT_COL = []
 
-    # profiler window dimensions, refresh rate, & bar length
-    self.height = 3
-    self.length = None
-    self.refresh = 0.5
-    self.ticks = 30
-
-    def __init__(self, enc="utf-8"):
+    def __init__(self, rows=3, refresh=0.5, ticks=20, enc="utf-8"):
         """
         Allocate datastructures and sys info
         """
 
+        # setup stdout wrapper
+        self.out = StdOutWrapper()
+        PerformanceProfiler.sys.stdout = self.out
+        PerformanceProfiler.sys.stderror = self.out
+
         # grab logical core count for profiler window
         self.enc = enc
-        self.NUM_CORE = int(check_output(self.NUM_CORE, encoding=self.enc))
+        self.NUM_CORE = int(
+            PerformanceProfiler.check_output(self.NUM_CORE, encoding=self.enc)
+        )
 
-        # setup stdout wrapper
-        self.stdout = StdOutWrapper()
-        stdout = self.stdout
+        # profiler window dimensions & refresh rate
+        self.rows = rows
+        self.refresh = refresh
+        self.ticks = ticks
 
-        # setup profiler data buffers
-        self.cols = math.ceil((self.NUM_CORE + len(self.MEM_STAT)) / self.height)
-        self.prof_buffer = [[""] * self.height] * self.cols
+        # setup 1:1 profiler data and label buffers
+        self.cols = PerformanceProfiler.ceil(
+            (self.NUM_CORE + len(self.GPU_COL)) / self.rows
+        )
+        self.prof_buffer = [["" for r in range(self.cols)] for c in range(self.rows)]
+        stats = ["CPU" + str(c).ljust(2) for c in range(self.NUM_CORE)] + self.GPU_COL
 
-        # setup label buffers to be 1:1 to data buffers
-        j = 0
-        for i in range(self.NUM_CORE // self.height):
-            self.STAT_COL.append(["CPU" + c for c in range(j, i + j)])
-            j = i
-        self.STAT_COL.append(self.GPU_COL)
+        # ensure buffer sizes match - format label buffers to be 1:1 to profiler buffer
+        stats += ["" for d in range(self.cols * self.rows - len(stats))]
+        self.STAT_COL = [
+            [stats[c + r * self.cols] for c in range(self.cols)]
+            for r in range(self.rows)
+        ]
 
-        # initialize curses and record terminal size
-        self.term_win = curses.initscr()
-        self.length = curses.COL
+        # initialize curses and record terminal size (hide cursor)
+        self.term_win = PerformanceProfiler.curses.initscr()
+        self.length = PerformanceProfiler.curses.COLS
+        PerformanceProfiler.curses.curs_set(0)
 
         # initialize profiler windows
-        profiler_box = curses.newwin(self.height + 2, self.length)
+        profiler_box = PerformanceProfiler.curses.newwin(self.rows + 2, self.length)
         profiler_box.box()
-        self.prof_win = profiler_box.derwin(self.height, self.length - 2, 1, 1)
+        self.prof_win = profiler_box.derwin(self.rows, self.length - 2, 1, 1)
 
         # initialize stdout windows
-        stdout_box = curses.newwin(curses.LINES - self.height, self.length, height, 0)
+        stdout_box = PerformanceProfiler.curses.newwin(
+            PerformanceProfiler.curses.LINES - self.rows - 2,
+            self.length,
+            self.rows + 2,
+            0,
+        )
         stdout_box.box()
         self.out_win = stdout_box.derwin(
-            curses.LINES - self.height - 2, self.length - 2, 1, 1
+            PerformanceProfiler.curses.LINES - self.rows - 4, self.length - 2, 1, 1
         )
         self.out_win.scrollok(True)
 
@@ -84,14 +95,14 @@ class PerformanceProfiler:
         stdout_box.refresh()
         return
 
-    def deinit():
+    def deinit(self):
         """
         Unloads curses library and restores terminal settings
         """
-        pass
-        return
+        PerformanceProfiler.stdout = PerformanceProfiler.sys.__stdout__
+        return PerformanceProfiler.curses.endwin()
 
-    def stdout():
+    def stdout(self):
         """
         Parses anything ready for us on STDOUT
         """
@@ -99,10 +110,9 @@ class PerformanceProfiler:
         # call wrapper and add to curses window
         self.out_win.addstr(self.stdout.read())
         self.out_win.refresh()
-        self.stdout.clear()
-        return
+        return self.stdout.clear()
 
-    def mem_utilization():
+    def mem_utilization(self):
         """
         Compute memory utilization from free -m
         """
@@ -110,10 +120,11 @@ class PerformanceProfiler:
         # run RAM command iteratively
         out = ""
         for command in self.MEM_STAT:
-            out = check_output(command, input=out)
-        return int(out[0]) / int(out[1])
+            out = PerformanceProfiler.check_output(command, input=out)
+        used, total = out.decode(self.enc).split()
+        return int(used) / int(total)
 
-    def cpu_utilization(prior_metrics):
+    def cpu_utilization(self, prior_metrics):
         """
         Compute CPU utilization from /proc/stat via:
         total = user + nice + system + idle + iowait + irq + softirq + steal
@@ -125,13 +136,14 @@ class PerformanceProfiler:
         # run CPU command iteratively
         out = ""
         for command in self.CPU_STAT:
-            out = check_output(command, input=out)
+            out = PerformanceProfiler.check_output(command, input=out)
 
-        # compute performance difference from old and new
-        metrics = array(
-            [metric.split(" ") for metric in out.decode(self.enc)], dtype=int
+        # compute performance delta
+        metrics = self.array(
+            [metric.split(" ") for metric in out.decode(self.enc).splitlines()],
+            dtype=int,
         )
-        user, nice, system, idle, iowait, irq, softirq, steal = subtract(
+        user, nice, system, idle, iowait, irq, softirq, steal = PerformanceProfiler.subtract(
             metrics, prior_metrics
         ).T
         total = user + nice + system + idle + iowait + irq + softirq + steal
@@ -140,12 +152,12 @@ class PerformanceProfiler:
 
         # on the first run, we can sometimes divide by zero
         try:
-            cpu_util = list(nan_to_num(usage / total))
+            cpu_util = list(PerformanceProfiler.nan_to_num(usage / total))
         except:
             pass
         return cpu_util, metrics
 
-    def gpu_utilization():
+    def gpu_utilization(self):
         """
         Compute GPU utilziation from nvidia-smi
         """
@@ -153,12 +165,12 @@ class PerformanceProfiler:
         # run GPU command iteratively
         out = ""
         for command in self.GPU_STAT:
-            out = check_output(command, input=out)
+            out = PerformanceProfiler.check_output(command, input=out)
         temp, mem_used, mem_total, gpu_util = out.decode(self.enc).split(" ")
         mem_util = int(mem_used.strip("MiB")) / int(mem_total.strip("MiB"))
-        return temp, mem_util, gpu_util
+        return temp, mem_util, gpu_util[:-1]  # (strip newline)
 
-    def compute_bars(metrics, tweak=1.03):
+    def compute_bars(self, metrics):
         """
         Return bars for the utilization of a given metrics
         """
@@ -166,58 +178,60 @@ class PerformanceProfiler:
         # compute bars for each metric
         elements_placed = 0
         for metric in metrics:
-            for m in metric:
 
-                # do not compute bars for string-typed metrics (ie temperature)
-                if not isinstance(m, str):
-                    self.prof_buffer[elements_placed % self.cols][
-                        elements_placed // self.cols
-                    ] = (
-                        "["
-                        + "|" * int(self.ticks * m)
-                        + " " * int(self.ticks * (tweak - m))
-                        + "]"
-                    )
-                else:
-                    self.prod_buffer[elements_placed % self.cols][
-                        elements_placed // self.cols
-                    ] = m
-                elements_placed += 1
+            # do not compute bars for string-typed metrics (ie temperature)
+            if not isinstance(metric, str):
+                self.prof_buffer[elements_placed // self.cols][
+                    elements_placed % self.cols
+                ] = ("[" + ("|" * int(self.ticks * metric)).ljust(self.ticks) + "]")
+            else:
+                self.prof_buffer[elements_placed // self.cols][
+                    elements_placed % self.cols
+                ] = metric.ljust(self.ticks + 4 - len(metric))
+            elements_placed += 1
         return
 
-    def render_bars(cpu, gpu_mem):
+    def render_bars(self):
         """
         Writes bars and refreshes curses profile window
         """
 
         # write bars to curses profile window
-        for i in range(self.height):
+        for i in range(self.rows):
             self.prof_win.addstr(
-                [
-                    " ".join(label, metric)
-                    for label, metric in zip(self.STAT_COL[i], self.prof_buffer[i])
-                ]
+                i,
+                0,
+                " ".join(
+                    [
+                        " ".join((label, metric))
+                        for label, metric in zip(self.STAT_COL[i], self.prof_buffer[i])
+                    ]
+                ),
             )
 
         # refresh curses profile window
-        self.prof_win.refresh()
-        return
+        return self.prof_win.refresh()
 
-    def profile():
+    def profile(self):
         """
         Main profiler function that obtains system metrics
         """
 
         # do some prep for the first update
         cpu_util, prior_cpu_util = self.cpu_utilization([[1] * 8] * self.NUM_CORE)
-        update = time()
+        update = PerformanceProfiler.time()
 
         # check if we've sent the stop signal
         while self.run_thread:
 
             # parse anything queued from STDOUT
-            self.stdout()
-            if time() - update >= self.refresh:
+            out = self.out.read()
+            if out:
+                self.out_win.addstr(out)
+                self.out_win.refresh()
+
+            # refresh the performance montior if applicable
+            if PerformanceProfiler.time() - update >= self.refresh:
 
                 # obtain metrics
                 mem_util = self.mem_utilization()
@@ -226,26 +240,27 @@ class PerformanceProfiler:
 
                 # compute bars and render
                 bars = self.compute_bars(
-                    [cpu_util, [gpu_util, gpu_mem, gpu_temp, mem_util]]
+                    cpu_util + [gpu_util, gpu_mem, gpu_temp, mem_util]
                 )
 
                 # render bars (and auxiliary measurements)
-                self.render_bars(win, bars[0], [bars[3], gpu_temp, bars[1], bars[2]])
-                update = time()
+                self.render_bars()
+                update = PerformanceProfiler.time()
         return
 
-    def start():
+    def start(self):
         """
         Starts profiler thread
         """
 
         # start in daemon mode in case things get wonky
-        self.profile_thread = Thread(target=profile, args=(), daemon=True)
+        self.profile_thread = PerformanceProfiler.Thread(
+            target=self.profile, daemon=True
+        )
         self.run_thread = True
-        self.profile_thread.start()
-        return
+        return self.profile_thread.start()
 
-    def stop():
+    def stop(self):
         """
         Stops profiler thread
         """
@@ -263,7 +278,7 @@ class StdOutWrapper:
 
     # initilize queue for STDOUT
     def __init__(self):
-        self.queue = deque()
+        self.queue = StdOutWrapper.deque()
         return
 
     # add STDOUT elements to queue
@@ -271,14 +286,11 @@ class StdOutWrapper:
         self.queue.append(out)
         return
 
-    # "pop" STDOUT elements from queue (pair with clear())
+    # "pop" STDOUT elements from queue and clear
     def read(self):
-        return list(self.queue)
-
-    # clear all elements from queue
-    def clear(self):
+        out = " ".join(list(self.queue))
         self.queue.clear()
-        return
+        return out
 
 
 if __name__ == "__main__":
@@ -287,15 +299,21 @@ if __name__ == "__main__":
     """
 
     from random import choice, randint
-    from time import time
+    from time import sleep
 
-    # initialize curses
-    stdscr = curses.initscr()
+    # instantiate profiler object and start
+    profiler = PerformanceProfiler()
+    profiler.start()
 
     # print some random words
     word_file = "/usr/share/dict/words"
     words = open(word_file).read().splitlines()
     while True:
-        print(" ".join(choice(words) for i in range(randint(1, 30))))
-        time.sleep(1)
+        try:
+            print(" ".join(choice(words) for i in range(randint(1, 30))))
+            sleep(1)
+        except:
+            profiler.stop()
+            profiler.deinit()
+            raise SystemExit(-1)
     raise SystemExit(0)
